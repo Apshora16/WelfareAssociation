83d5e73d4a2f0ee6347f7298c6aa33f1d9b0f1b7
2312016jerin <2312016@iub.edu.bd>
2026-08-08 21:12:02 +0600

    finish

--- src/main/java/com/example/welfareassociation/Treasurer/CollectioncontributionsController.java
+++ src/main/java/com/example/welfareassociation/Treasurer/CollectioncontributionsController.java
@@ -3,25 +3,5 @@ package com.example.welfareassociation.Treasurer;
 public class CollectioncontributionsController
 {
     @javafx.fxml.FXML
-    private ComboBox contributionTypeComboBox;
-    @javafx.fxml.FXML
-    private ComboBox paymentMethodComboBox;
-    @javafx.fxml.FXML
-    private TextField memberIdField;
-    @javafx.fxml.FXML
-    private TextField amountField;
-    @javafx.fxml.FXML
-    private TextArea receiptTextArea;
-
-    @javafx.fxml.FXML
     public void initialize() {
-    }
-
-    @javafx.fxml.FXML
-    public void handledisplayrecepitButtton(ActionEvent actionEvent) {
-    }
-
-    @javafx.fxml.FXML
-    public void handlesavecontributionButton(ActionEvent actionEvent) {
-    }
-}
+    }}
---
+++ src/main/java/com/example/welfareassociation/WelfareOfficer/Welfarereports.java
@@ -0,0 +1,4 @@
+package com.example.welfareassociation.WelfareOfficer;
+
+public class Welfarereports {
+}
